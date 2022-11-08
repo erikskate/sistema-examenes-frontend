6fe0522164d3ae2ab1ae2c132c3b36afea1586c5
erikskate <erikjoel35@hotmail.com>
2022-11-07 18:34:32 -0600

    Se agrega el formulario de login, se configuran las rutas y se agrega el homeComponent

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public user = {
+    username : '',
+    password : '',
+    nombre : '',
+    apellido : '',
+    email: '',
+    telefono: ''
+  }
+
+  constructor(private userService:UserService, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+  }
+
+  formSubmit(){
+    console.log(this.user);
+    if(this.user.username === '' || this.user.username == null){
+      this.snackBar.open("El nombre de usuario es requerido.",'Aceptar',{
+        duration: 3000,
+        verticalPosition : 'top',
+        horizontalPosition: 'right'
+      });
+      return;
+    }
+
+    this.userService.añadirUsuario(this.user).subscribe(data => {
+      Swal.fire('Usuario guardado','Usuario registrado con exito en el sistema','success');
+    },
+    (error) => {
+      this.snackBar.open("Ha ocurrido un error en el sistema.",'Aceptar',{
+        duration: 3000
+      });
+    });
   }
 
 }
